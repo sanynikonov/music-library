9f23ca36757a80361b8e38d64c13fbb883fe1146
Oleksandr Nikonov <nikonov.sanynikonov@gmail.com>
2021-12-19 19:26:07 +0200

    Add auth service

---
+++ MusicLibrary.Business/Interfaces/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.Business
+{
+    public interface IAuthService
+    {
+        Task<SignInResult> LoginAsync(LoginModel model);
+        Task<SignInResult> TryLoginAsync(LoginModel model);
+        Task<IdentityResult> RegisterAsync(RegisterModel model);
+        string GetUserId(ClaimsPrincipal principal);
+        Task LogoutAsync();
+    }
+}
---
+++ MusicLibrary.Business/Services/AuthService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MusicLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.Business
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUnitOfWork _unit;
+
+        public AuthService(IUnitOfWork unit)
+        {
+            _unit = unit;
+        }
+
+        public string GetUserId(ClaimsPrincipal principal)
+        {
+            return _unit.SignInManager.UserManager.GetUserId(principal);
+        }
+
+        public async Task<SignInResult> TryLoginAsync(LoginModel model)
+        {
+            var user = await _unit.UserManager.FindByNameAsync(model.Login) ?? await _unit.UserManager.FindByEmailAsync(model.Login);
+            return await _unit.SignInManager.CheckPasswordSignInAsync(user, model.Password, lockoutOnFailure: false);
+        }
+
+
+        public async Task<SignInResult> LoginAsync(LoginModel model)
+        {
+            var user = await _unit.UserManager.FindByNameAsync(model.Login) ?? await _unit.UserManager.FindByEmailAsync(model.Login);
+            return await _unit.SignInManager.PasswordSignInAsync(user.UserName, model.Password, isPersistent: false, lockoutOnFailure: false);
+        }
+
+        public async Task LogoutAsync()
+        {
+            await _unit.SignInManager.SignOutAsync();
+        }
+
+        public async Task<IdentityResult> RegisterAsync(RegisterModel model)
+        {
+            var user = new User
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+            };
+
+            return await _unit.SignInManager.UserManager.CreateAsync(user, model.Password);
+        }
+    }
+}
