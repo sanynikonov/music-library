20e23d8fb3b5cc1ae45acfc559e1d0f85c048416
Oleksandr Nikonov <nikonov.sanynikonov@gmail.com>
2021-12-20 02:38:20 +0200

    Add jwt auth beginning

---
+++ MusicLibrary.Business/Interfaces/IJwtService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.Business
+{
+    public interface IJwtService
+    {
+        string CreateToken(string username);
+    }
+}
---
+++ MusicLibrary.Web/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicLibrary.Business;
+
+namespace MusicLibrary.Web.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _service;
+
+        public AuthController(IAuthService service)
+        {
+            _service = service;
+        }
+    }
+}
