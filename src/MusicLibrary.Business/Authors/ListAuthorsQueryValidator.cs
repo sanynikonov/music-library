e50f8186f519bc01eb731822207c4523e8925e57
Oleksandr Nikonov <nikonov.sanynikonov@gmail.com>
2022-04-12 00:42:32 +0300

    Add validation

---
+++ src/MusicLibrary.Business/Authors/ListAuthorsQueryValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace MusicLibrary.Business.Authors;
+
+public class ListAuthorsQueryValidator : AbstractValidator<ListAuthorsQuery>
+{
+    public ListAuthorsQueryValidator()
+    {
+        RuleFor(q => q.PageSize)
+            .GreaterThan(0)
+            .WithMessage("Page size must be greater than 0");
+
+        RuleFor(q => q.PageNumber)
+            .GreaterThan(0)
+            .WithMessage("Page number must be greater than 0");
+
+        RuleFor(q => q.SearchString)
+            .NotEmpty()
+            .WithMessage("Search string must not be empty");
+    }
+}
