202fc0fb9f40f617bc2aca90ba1c5323e62d0c69
Oleksandr Nikonov <nikonov.sanynikonov@gmail.com>
2022-04-16 13:50:41 +0300

    Add infrastructure

---
+++ samples/MigrationTest/DbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using MusicLibrary.Infrastructure;
+
+namespace MigrationTest;
+
+public class DbContextFactory : IDesignTimeDbContextFactory<MusicLibraryContext>
+{
+    public MusicLibraryContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<MusicLibraryContext>();
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MusicLibraryDomain;Integrated Security=True;");
+
+        return new MusicLibraryContext(optionsBuilder.Options);
+    }
+}
---
+++ src/MusicLibrary.Domain/Entities/Artists/Artist.cs
@@ -0,0 +1,18 @@
+﻿using MusicLibrary.Domain.Common;
+using MusicLibrary.Domain.Entities.Releases;
+
+namespace MusicLibrary.Domain.Entities.Artists;
+
+public class Artist : BaseEntity, IAggregateRoot
+{
+    private readonly List<Release> _releases = new();
+
+    public string Name { get; private set; }
+    public string LogoPath { get; private set; }
+    public string Bio { get; private set; }
+
+    public Artist()
+    {
+
+    }
+}
