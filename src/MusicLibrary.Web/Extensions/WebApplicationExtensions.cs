baab859c81fcc51be455b2995d1de5836a140530
Oleksandr Nikonov <nikonov.sanynikonov@gmail.com>
2022-05-21 23:17:59 +0300

    Add more initial data

--- src/MusicLibrary.Web/Extensions/WebApplicationExtensions.cs
+++ src/MusicLibrary.Web/Extensions/WebApplicationExtensions.cs
@@ -8,17 +8,17 @@ public static class WebApplicationExtensions
 {
     public static async Task<WebApplication> SeedData(this WebApplication host)
     {
-        using (var scope = host.Services.CreateScope())
+        using var scope = host.Services.CreateScope();
+
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<MusicLibraryContext>();
+
+        if (!context.Users.Any())
         {
-            var services = scope.ServiceProvider;
-            var context = services.GetRequiredService<MusicLibraryContext>();
-
-            if (context.Users.Any()) return host;
-
             var roleManager = services.GetRequiredService<RoleManager<Role>>();
-            var adminRole = new Role {Name = "admin"};
-            var authorRole = new Role {Name = "author"};
-            var userRole = new Role {Name = "user"};
+            var adminRole = new Role { Name = "admin" };
+            var authorRole = new Role { Name = "author" };
+            var userRole = new Role { Name = "user" };
             await roleManager.CreateAsync(adminRole);
             await roleManager.CreateAsync(authorRole);
             await roleManager.CreateAsync(userRole);
@@ -26,17 +26,26 @@ public static class WebApplicationExtensions
             var userManager = services.GetRequiredService<UserManager<User>>();
             var admin = new User
             {
-                Email = "admin@ukr.net", UserName = "admin", Name = "Test Admin", Bio = "Test Admin",
+                Email = "admin@ukr.net",
+                UserName = "admin",
+                Name = "Test Admin",
+                Bio = "Test Admin",
                 ProfilePicturePath = ""
             };
             var author = new User
             {
-                Email = "author@ukr.net", UserName = "author", Name = "Test Artist", Bio = "Test Artist",
+                Email = "author@ukr.net",
+                UserName = "author",
+                Name = "Test Artist",
+                Bio = "Test Artist",
                 ProfilePicturePath = ""
             };
             var user = new User
             {
-                Email = "user@ukr.net", UserName = "user", Name = "Test User", Bio = "Test User",
+                Email = "user@ukr.net",
+                UserName = "user",
+                Name = "Test User",
+                Bio = "Test User",
                 ProfilePicturePath = ""
             };
             await userManager.CreateAsync(admin);
@@ -46,6 +55,94 @@ public static class WebApplicationExtensions
             await userManager.AddToRoleAsync(admin, adminRole.Name.ToUpper());
             await userManager.AddToRoleAsync(author, authorRole.Name.ToUpper());
             await userManager.AddToRoleAsync(user, userRole.Name.ToUpper());
+        }
+
+        if (!context.Collections.Any())
+        {
+            var ledZeppelin = new Artist
+            {
+                Name = "Led Zeppelin",
+                
+            };
+
+            var bestOfLedZeppelin = new Collection
+            {
+                Title = "Best of Led Zeppelin",
+                UserId = 3 // User
+            };
+
+            ledZeppelin.Releases.Add(new Collection
+            {
+                Title = "Led Zeppelin IV",
+                Type = ReleaseType.LongPlay,
+                Year = 1971,
+                UserId = 2, // Artist
+                Songs = new List<Song>
+                {
+                    new Song
+                    {
+                        Title = "Black Dog",
+                        Artists = new List<Artist> { ledZeppelin },
+                        Playlists = new List<Collection> { bestOfLedZeppelin }
+                    },
+                    new Song
+                    {
+                        Title = "Rock And Roll",
+                        Artists = new List<Artist> { ledZeppelin },
+                        Playlists = new List<Collection> { bestOfLedZeppelin }
+                    },
+                    new Song
+                    {
+                        Title = "The Battle of Evermore",
+                        Artists = new List<Artist> { ledZeppelin }
+                    },
+                    new Song
+                    {
+                        Title = "Stairway to Heaven",
+                        Artists = new List<Artist> { ledZeppelin },
+                        Playlists = new List<Collection> { bestOfLedZeppelin }
+                    },
+                    new Song
+                    {
+                        Title = "Misty Mountain Hop",
+                        Artists = new List<Artist> { ledZeppelin }
+                    },
+                    new Song
+                    {
+                        Title = "Four Sticks",
+                        Artists = new List<Artist> { ledZeppelin }
+                    },
+                    new Song
+                    {
+                        Title = "Going to California",
+                        Artists = new List<Artist> { ledZeppelin }
+                    },
+                    new Song
+                    {
+                        Title = "When the Levee Breaks",
+                        Artists = new List<Artist> { ledZeppelin },
+                        Playlists = new List<Collection> { bestOfLedZeppelin }
+                    }
+                }
+            });
+            ledZeppelin.Releases.Add(new Collection
+            {
+                Title = "Black Dog",
+                Type = ReleaseType.Single,
+                Year = 1971,
+                UserId = 2,
+                Songs = new List<Song>
+                {
+                    new Song
+                    {
+                        Title = "Black Dog",
+                        Artists = new List<Artist> { ledZeppelin }
+                    }
+                }
+            });
+
+            await context.Artists.AddAsync(ledZeppelin);
+            await context.SaveChangesAsync();
         }
 
         return host;
